38cdaf25c403bf0a307051b7433832c01ed26305
Anton Pan <antoxaantoxa23@gmail.com>
2022-12-14 21:17:14 +0600

    Added longer path and move animations

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,6 +5,13 @@ public class PlayerControls : MonoBehaviour
     [SerializeField] private float _controlSpeed;
     [SerializeField] private float _xRange;
     [SerializeField] private float _yRange;
+    [SerializeField] private float _pitchFactor;
+    [SerializeField] private float _controlPitchFactor;
+    [SerializeField] private float _positionYawFactor;
+    [SerializeField] private float _controlRollFactor;
+
+    float yThrow;
+    float xThrow;
 
     void Update()
     {
@@ -14,8 +21,8 @@ public class PlayerControls : MonoBehaviour
 
     private void ProcessTranslation()
     {
-        float xThrow = Input.GetAxis("Horizontal");
-        float yThrow = Input.GetAxis("Vertical");
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
 
         float xOffset = xThrow * Time.deltaTime * _controlSpeed;
         float rawXPos = transform.localPosition.x + xOffset;
@@ -30,6 +37,13 @@ public class PlayerControls : MonoBehaviour
 
     private void ProcessRotation()
     {
-        transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        float pitchDueToPosition = transform.localPosition.y * -_pitchFactor;
+        float pitchDueToControl = yThrow * -_controlPitchFactor;
+
+        float pitch = pitchDueToPosition + pitchDueToControl;
+        float yaw = transform.localPosition.x * _positionYawFactor;
+        float roll = transform.localRotation.z + xThrow * -_controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
